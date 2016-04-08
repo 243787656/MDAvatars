cf96d8e3290d3d2aaa0bedd874ff3c4b30cae450
AlexLionne <alextitou3@gmail.com>
2016-04-08 02:15:37 +0200

    adding toolbar to the settings

--- app/src/main/java/com/alexlionne/apps/avatars/SettingsActivity.java
+++ app/src/main/java/com/alexlionne/apps/avatars/SettingsActivity.java
@@ -3,6 +3,10 @@ package com.alexlionne.apps.avatars;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.alexlionne.apps.avatars.R;
 
@@ -19,7 +23,16 @@ public class SettingsActivity extends PreferenceActivity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences_settings);
-    }
 
+        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
 }
